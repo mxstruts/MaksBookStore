dc361de0fbfa5192a4008a90ecb604f4c2a23093
mxstruts <shatilo.max@gmail.com>
2023-10-30 17:49:23 -0400

    Initiliazed Project.

--- MaksBookStore/Startup.cs
+++ MaksBookStore/Startup.cs
@@ -32,7 +32,7 @@ namespace MaksBookStore
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
