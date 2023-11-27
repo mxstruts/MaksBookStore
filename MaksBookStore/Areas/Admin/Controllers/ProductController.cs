7697f647021d44e3c6fd422f09fecb2b53fcfcdd
mxstruts <shatilo.max@gmail.com>
2023-11-27 13:35:06 -0500

    Fixed product index loading

--- MaksBookStore/Areas/Admin/Controllers/ProductController.cs
+++ MaksBookStore/Areas/Admin/Controllers/ProductController.cs
@@ -93,7 +93,7 @@ namespace MaksBookStore.Areas.Admin.Controllers
         public IActionResult GetAll()
         {
             //return NotFound();
-            var allObj = _unitOfWork.Product.GetAll(includeProperties: "Category, CoverType");
+            var allObj = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
             return Json(new { data = allObj });
         }
 
